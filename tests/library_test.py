62f45613725977e90ab50d5f00606def8fc90f4e
David Good <dgood@programminggoody.com>
2025-02-05 11:45:05 -0500

    updated tests

--- tests/library_test.py
+++ tests/library_test.py
@@ -1,42 +1,48 @@
 from assertpy import assert_that
 
-from library_functions.library_operations import add_book, remove_book, mark_as_read, get_books_by_author
+from library_functions.library_operations import Library
 
 
 def test_add_book():
-    library = {}
+    library = Library()
     book = {"title": "Book One", "author": "Author A", "year": 2000, "read_status": False}
-    add_book(library, book)
+    library.add_book(book)
     assert_that(library).contains("Book One")
     assert_that(library["Book One"]).is_equal_to(
         {"title": "Book One", "author": "Author A", "year": 2000, "read_status": False})
 
 
 def test_remove_book():
-    library = {"Book One": {"title": "Book One", "author": "Author A", "year": 2000, "read_status": False}}
-    remove_book(library, "Book One")
+    library = Library()
+    book = {"title": "Book One", "author": "Author A", "year": 2000, "read_status": False}
+    library.add_book(book)
+    library.remove_book("Book One")
     assert_that(library).does_not_contain("Book One")
 
 
 def test_mark_as_read():
-    library = {"Book One": {"title": "Book One", "author": "Author A", "year": 2000, "read_status": False}}
-    mark_as_read(library, "Book One")
+    library = Library()
+    library.add_book({"Book One": {"title": "Book One", "author": "Author A", "year": 2000, "read_status": False}})
+    library.mark_as_read("Book One")
     assert_that(library["Book One"]["read_status"]).is_true()
 
 
 def test_get_books_by_author():
-    library = {
+    library = Library()
+    books = {
         "Book One": {"title": "Book One", "author": "Author A", "year": 2000, "read_status": False},
         "Book Two": {"title": "Book Two", "author": "Author B", "year": 2001, "read_status": True},
         "Book Three": {"title": "Book Three", "author": "Author A", "year": 2002, "read_status": False},
     }
-    books_by_author_a = get_books_by_author(library, "Author A")
+    for books in books.values():
+        library.add_book(books)
+    books_by_author_a = library.get_books_by_author("Author A")
     assert_that(books_by_author_a).contains_only("Book One", "Book Three")
 
 
 def test_get_books_by_author_no_match():
-    library = {
-        "Book One": {"title": "Book One", "author": "Author A", "year": 2000, "read_status": False},
-    }
-    books_by_author_c = get_books_by_author(library, "Author C")
+    library = Library()
+    book = {"title": "Book One", "author": "Author A", "year": 2000, "read_status": False}
+    library.add_book(book)
+    books_by_author_c = library.get_books_by_author("Author C")
     assert_that(books_by_author_c).is_empty()
